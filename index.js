5ed9fe16b28b152cad936969992b1893123b83f5
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-30 14:49:34 +0100

    Add UI class with fake hardcoded database and display the list

--- index.js
+++ index.js
@@ -4,3 +4,48 @@ class Company {
         this.url = url;
     }
 }
+
+class UI {
+    static displayCompanies() {
+        // Fake database
+        const StoredCompanies = [
+            {
+               "url":"https://www.10clouds.com",
+               "name":"10Clouds"
+            },
+            {
+               "url":"https://2cW.pl",
+               "name":"2cW"
+            },
+            {
+               "url":"https://www.2n.pl/",
+               "name":"2N"
+            },
+            {
+               "url":"https://3step.pl/",
+               "name":"3Step"
+            }
+        ];
+
+        const companies = StoredCompanies;
+
+        companies.forEach(company => {
+            UI.addCompanyToList(company);
+        });
+    }
+
+    static addCompanyToList(company) {
+        const list = document.querySelector('.companies-list');
+
+        const listItem = document.createElement('li');
+
+        listItem.innerHTML = `
+            <li><a href="${company.url}">${company.name}</a></li>
+        `;
+
+        list.appendChild(listItem);
+    }
+}
+
+// Display Companies
+document.addEventListener('DOMContentLoaded', UI.displayCompanies);
